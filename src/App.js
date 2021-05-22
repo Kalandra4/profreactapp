267fc8280192c0fe67e5bc8e4630de7a8fc38584
Kalandra4 <kalandraawc@gmail.com>
2021-05-22 15:33:57 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
+import Display from "./Components/Display";
+import { useState } from "react";
+import me from "./images/me.jpg";
 import './App.css';
 
 function App() {
+
+  const [name, setName] = useState("");
+
+  const clickName = () => {
+    setName("My Name is Kalandra.");
+  };
+
+  const clickSkills = () => {
+    setName("I am a Front-End Developer.");
+  };
+
+  const [count, setCount] = useState(0);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <section className="card">
+        <img src={me} alt="Kalandra" className="profileImg"/>
+        <Display myname={name} />
+        <button
+          className="btn"
+          onClick={clickName}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Name
+        </button>
+
+        {/* Uppercase = CONTENT */}
+        {/* lowercase = content */}
+        {/* camelCase = contentType */}
+        {/* PascalCase = ContentType */}
+
+        {/* DRY - Don't Repeat Yourself */}
+        {/* DIE - Duplicate Is Evil */}
+
+        {/* <Button onClickHandler={clickName} btnText={Name} />
+        <Button onClickHandler={clickSkills} btnText={Skills} /> */}
+
+        <button className="btn" onClick={clickSkills}>Skills</button>
+        <div>
+          <Display myname={count} />
+          {/* <Button onClickHandler={setCount} /> */}
+          <button className="btn-like" onClick={() => setCount(count + 1)}>
+            <i className="fas fa-heart fa-2x"></i>
+          </button>
+        </div>
+        <footer className="social-link">
+          <a 
+            href="https://twitter.com/kalandra67"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <i class="fab fa-twitter fa-2x"></i>
+          </a>
+        </footer>
+      </section>
+    </>
   );
 }
 
---
+++ src/Components/Display.js
@@ -0,0 +1,21 @@
+const Display = ({myname}) => {
+  return ( 
+    <>
+      <div className="content">{myname}</div>
+    </>
+   );
+}
+ 
+export default Display;
+
+
+
+// const Button = ({onClickHandler, btnText}) => {
+//   return ( 
+//     <>
+//       <button className="btn" onClick={onClickHandler}>{btnText}</button>
+//     </>
+//    );
+// }
+ 
+// export default Button;
